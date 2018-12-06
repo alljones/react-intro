3589abe492a022a4c874c822951ec7a4b664284f
Allan Jones <iamallanjones@gmail.com>
2018-12-06 03:16:12 -0600

    Board controls Square Component's State

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,20 +4,20 @@ import './index.css';
 
 
 class Square extends React.Component {
-    constructor(props) {
+    /* constructor(props) {
         super(props);
         this.state = {
           value: null,
         };
-      }
+      } */
 
     render() {
       return (
         <button 
             className="square" 
-            onClick={() => this.setState({value: 'X'})}
+            onClick={() => this.props.onClick()}
         >
-          { this.state.value }
+          { this.props.value }
         </button>
       );
     }
@@ -30,9 +30,18 @@ class Square extends React.Component {
           squares: Array(9).fill(null),
         };
       }
+
+      handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = 'X';
+        this.setState({squares: squares});
+      }
       
     renderSquare(i) {
-        return <Square value={this.state.squares[i]} />;
+        return <Square 
+            value={this.state.squares[i]}
+            onClick={() => this.handleClick(i)} 
+        />;
     }
   
     render() {
